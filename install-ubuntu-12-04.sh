d44bb5083c19a46164d6a12e2ec60b83dea15ab1
devmario <mario@vanillabreeze>
2013-02-06 09:26:46 +0000

    install.sh changed

--- install-ubuntu-12-04.sh
+++ install-ubuntu-12-04.sh
@@ -1,7 +1,14 @@
-sudo chmod 755 jsawk && sudo mv jsawk /usr/bin/jsawk
+sudo chmod 755 jsawk && sudo cp jsawk /usr/bin/jsawk
 sudo apt-get install unzip
 wget http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/jsshell-linux-x86_64.zip
 unzip -d jsshell jsshell-linux-x86_64.zip
+sudo chmod 755 jsshell/js
+sudo chmod 755 jsshell/libnspr4.so
+sudo chmod 755 jsshell/libplds4.so
+sudo chmod 755 jsshell/libplc4.so
+sudo cp jsshell/libnspr4.so /usr/lib/libnspr4.so
+sudo cp jsshell/libplds4.so /usr/lib/libplds4.so
+sudo cp jsshell/libplc4.so /usr/lib/libplc4.so
 sudo cp jsshell/js /usr/bin/js
 sudo chmod 755 /usr/bin/js
 rm -rf jsshell jsshell-linux-x86_64.zip
